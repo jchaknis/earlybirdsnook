780e3fb75b180faf21e16bd48451e6cfa14e6dc9
Jennifer Chaknis <jenniferchaknis40@gmail.com>
2014-10-11 15:40:29 -0400

    Create a rails application and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earlybirdsnook::Application.config.secret_key_base = '63e4a0df647d1bd24380da066b5fb1daa7af66e91b2f6053ab00c1ef49535ddb51c0a09ae8baad18b7a5da2534f4a0ee3dbdf6bb41bfd16a186edecf27898168'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Earlybirdsnook::Application.config.session_store :cookie_store, key: '_earlybirdsnook_session'
